a891f6097e6b001135dc56729323188acf122869
Tsynkush Andrey <AndreyCyn@gmail.com>
2024-09-06 21:47:22 +0300

    Add basic route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layout');
 });
+
+Route::group(['namespace' => 'User'], function () {
+    Route::get('/users', [UserController::class, 'index']);
+});
+
